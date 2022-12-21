1c3fa2d9f2309292aca94528ded8edac37d3d389
Yuna <example@gmail.com>
2022-12-21 12:49:12 +0300

    menu cards realisation from local json database with the help of fetch, async-await....etc

--- js/script.js
+++ js/script.js
@@ -235,42 +235,27 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
-  свежих овощей и фруктов. Продукт активных и здоровых людей. Это
-  абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
-    9,
-    ".menu .container",
-    ".menu__item"
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    `В меню “Премиум” мы используем не только красивый дизайн упаковки,
-       но и качественное исполнение блюд. Красная рыба, морепродукты,
-       фрукты - ресторанное меню без похода в ресторан!
-`,
-    12,
-    ".menu .container"
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
+    return await res.json();
+  };
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    `Меню “Постное” - это тщательный подбор ингредиентов: полное
-    отсутствие продуктов животного происхождения, молоко из миндаля,
-    овса, кокоса или гречки, правильное количество белков за счет тофу
-    и импортных вегетарианских стейков.`,
-    11,
-    ".menu .container",
-    ".menu__item"
-  ).render();
+  getResource("http://localhost:3000/menu").then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
 
   //Отправка данных с форм fetch
 
@@ -282,27 +267,35 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: data,
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
       const formData = new FormData(form);
 
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+      //тоже самое
+      // const object = {};
+      // formData.forEach(function (value, key) {
+      //   object[key] = value;
+      // });
+      // после этого делали JSON.stringify(object) после урла в фэче в качестве дэйты
 
-      fetch("server.php", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(object),
-      })
-        .then((data) => data.text())
+      postData("http://localhost:3000/requests", json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
